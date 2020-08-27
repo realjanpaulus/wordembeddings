d021992afdd7522a57532a5eb57611474cebec3a
Jan Paulus <janpaulus95@web.de>
2020-08-27 10:52:37 +0200

    fix: acc

--- app/utils.py
+++ app/utils.py
@@ -150,6 +150,11 @@ def early_stopping(d, patience=2):
 		else:
 			return False
 
+def flat_accuracy(preds, labels):
+	pred_flat = np.argmax(preds, axis=1).flatten()
+	labels_flat = labels.flatten()
+	return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
 def flat_f1(true_labels, preds):
 	""" Flattens predictions and labels and omputes macro f1-score.
 	"""
